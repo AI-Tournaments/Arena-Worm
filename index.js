60485342f4672f8094a4a75cbbe646cf03b18a5a
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-09-16 08:22:52 +0200

    Flipped Z rotation.

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function a(){
 			});
 			if(replay.arenaResult.settings.arena.threeDimensions){
 				function angleChange(){
-					layerWrapper.style.transform = 'rotateX('+slider_rotateX.value+'deg) rotateZ('+slider_rotateZ.value+'deg)';
+					layerWrapper.style.transform = 'rotateX('+slider_rotateX.value+'deg) rotateZ('+-slider_rotateZ.value+'deg)';
 				}
 				function updateDragPos(mouseEvent) {
 					if(![slider, slider_rotateX, slider_rotateZ].includes(mouseEvent.srcElement)){
@@ -56,7 +56,7 @@ function a(){
 						let deltaY = dragPos.y - event.pageY;
 						updateDragPos(event);
 						slider_rotateX.valueAsNumber += deltaY;
-						slider_rotateZ.valueAsNumber += deltaX;
+						slider_rotateZ.valueAsNumber -= deltaX;
 						switch(slider_rotateZ.value){
 							case slider_rotateZ.max:
 								slider_rotateZ.value = slider_rotateZ.min;
