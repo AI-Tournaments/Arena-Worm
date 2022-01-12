40bac6bb8359cb1d9cf43df0711ec5386382c31f
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-01-12 12:39:52 +0100

    Removed the even amount of teams limit.

--- arena.js
+++ arena.js
@@ -606,9 +606,7 @@ function rotateArray(array){
 ArenaHelper.init = (participants, settings) => {
 	_participants = participants;
 	_settings = settings;
-	if(_participants.countTeams()%2 !== 0 && !(_settings.rules.winner === 'MostPoints' && _participants.countTeams() === 1)){
-		ArenaHelper.postAbort('', 'Uneven amount of teams is not supported, only MostPoints can be played with a single worm.');
-	}else if(_settings.arena.size%2 !== 1){
+	if(_settings.arena.size%2 !== 1){
 		ArenaHelper.postAbort('', 'Arena size has to be uneven.');
 	}else if(_settings.rules.winner === 'MostPoints' && _settings.rules.defeatedWorms !== 'Solid'){
 		ArenaHelper.postAbort('', 'Incompatible rules: MostPoints can only be played with Solid.');
